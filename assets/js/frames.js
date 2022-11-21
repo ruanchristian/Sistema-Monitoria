20a4deeffd0a7851745b6e3fd819c34f329831de
ruanchristian <christianruan94@gmail.com>
2022-11-20 21:11:29 -0300

    Conflitos CSS corrigidos

--- assets/js/frames.js
+++ assets/js/frames.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('#datatable').DataTable({
+        order: [[1, 'asc'], [2, 'asc']],
         language : {
             url: "https://cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json"
         }
